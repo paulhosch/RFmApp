29f1e7529568750ac1171a6fc800e06aa2e9256a
paulhosch <paul.christian.hosch@gmail.com>
2024-08-22 15:49:09 +0200

    :wastebasket: clear cache button added

--- pages/home.py
+++ pages/home.py
@@ -4,3 +4,8 @@ from backend.ee import initialize_rfmapp
 
 def home():
     initialize_rfmapp()
+
+    # Add a button to the Streamlit app
+    if st.button("Clear Cache"):
+        st.cache_data.clear()
+        st.success("Cache cleared!")
